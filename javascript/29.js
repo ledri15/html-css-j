0eeafd885ba47716209e8e0ab4d318bc51f59a8b
unknown <ledrineziri@gmail.com>
2025-01-16 16:15:13 +0100

    Create 29.js

---
+++ javascript/29.js
@@ -0,0 +1,9 @@
+programinglanguages.unshift('ruby on rails');
+programinglanguages.shift('ruby on rails');
+programinglanguages.splice('ruby');
+math.random
+
+var programinglanguages = ["Javascript","css","html"]
+console.log(math.random*[5]);
+
+var numbers = [1,2,3,4,5,6,7,8];
